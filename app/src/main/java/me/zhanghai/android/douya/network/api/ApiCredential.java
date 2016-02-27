3ade0449c012b6ab33417bd73e49c98f02d63706
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-02-27 22:52:17 +0800

    Add HackyApiCredentialHelper.

--- app/src/main/java/me/zhanghai/android/douya/network/api/ApiCredential.java
+++ app/src/main/java/me/zhanghai/android/douya/network/api/ApiCredential.java
@@ -5,6 +5,16 @@
 
 package me.zhanghai.android.douya.network.api;
 
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import me.zhanghai.android.douya.util.IoUtils;
+import me.zhanghai.android.douya.util.LogUtils;
+import me.zhanghai.android.douya.util.StandardCharsetsCompat;
+
 interface ApiCredential {
 
     interface Douya {
@@ -13,7 +23,27 @@ interface ApiCredential {
     }
 
     interface Frodo {
-        String KEY = "<KEY>";
-        String SECRET = "<SECRET>";
+        String KEY = HackyApiCredentialHelper.loadStringFromFile(new File(
+                Environment.getExternalStorageDirectory(), "Douya/API_KEY"));
+        String SECRET = HackyApiCredentialHelper.loadStringFromFile(new File(
+                Environment.getExternalStorageDirectory(), "Douya/API_SECRET"));
+    }
+}
+
+class HackyApiCredentialHelper {
+
+    public static String loadStringFromFile(File file) {
+        if (!file.exists()) {
+            LogUtils.e("File " + file + " does not exist.");
+            return "";
+        }
+        try {
+            return IoUtils.inputStreamToString(new FileInputStream(file),
+                    StandardCharsetsCompat.UTF_8.name())
+                    .trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
