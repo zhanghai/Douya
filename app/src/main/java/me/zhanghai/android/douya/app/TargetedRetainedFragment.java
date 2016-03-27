01f1f900880aaf2148396b341bfadec5e2e4818e
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-03-27 18:36:31 +0800

    Remove unused import.

--- app/src/main/java/me/zhanghai/android/douya/app/TargetedRetainedFragment.java
+++ app/src/main/java/me/zhanghai/android/douya/app/TargetedRetainedFragment.java
@@ -5,8 +5,6 @@
 
 package me.zhanghai.android.douya.app;
 
-import android.content.Context;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FriendlyFragment;
