b63007612af6ecb64cc2ba273f75f4eb733f608d
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-07-14 17:11:45 +0800

    Fix offset bug.

--- app/src/main/java/me/zhanghai/android/douya/profile/ui/ProfileLayout.java
+++ app/src/main/java/me/zhanghai/android/douya/profile/ui/ProfileLayout.java
@@ -99,7 +99,10 @@ public class ProfileLayout extends FlexibleSpaceLayout {
 
     public void offsetTo(int offset) {
 
-        if (offset < 0 || getOffset() == offset) {
+        if (offset < 0) {
+            offset = 0;
+        }
+        if (getOffset() == offset) {
             return;
         }
 
