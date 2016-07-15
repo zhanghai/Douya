3c53bbb7c8b92c8928e122278eaebeb02bddb93f
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-07-15 09:59:13 +0800

    Fabric is hard to configure correctly, leaving as the default.

--- app/src/main/java/me/zhanghai/android/douya/fabric/FabricUtils.java
+++ app/src/main/java/me/zhanghai/android/douya/fabric/FabricUtils.java
@@ -8,7 +8,6 @@ package me.zhanghai.android.douya.fabric;
 import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
 import me.zhanghai.android.douya.BuildConfig;
@@ -18,11 +17,9 @@ public class FabricUtils {
     private FabricUtils() {}
 
     public static void init(Context context) {
-        Crashlytics crashlytics = new Crashlytics.Builder()
-                .core(new CrashlyticsCore.Builder()
-                        .disabled(BuildConfig.DEBUG)
-                        .build())
-                .build();
-        Fabric.with(context, crashlytics);
+        if (BuildConfig.DEBUG) {
+            return;
+        }
+        Fabric.with(context, new Crashlytics());
     }
 }
