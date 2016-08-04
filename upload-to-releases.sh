52c3639a5d1b841c3db98aae9fa0823e8138401a
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-08-04 23:55:12 +0800

    Make curl verbose for debugging.

--- upload-to-releases.sh
+++ upload-to-releases.sh
@@ -9,7 +9,7 @@ body="$(git log -n 1)"
 
 # Get old release by tag
 echo "Getting old release by tag..." >&2
-response="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/repos/${REPO}/releases/tags/${tag}")"
+response="$(curl -v -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/repos/${REPO}/releases/tags/${tag}")"
 echo "${response}" >&2
 old_release_id="$(echo "${response}" | jq -r '.id')"
 
@@ -17,13 +17,13 @@ if [[ "${old_release_id}" != "null" ]]; then
 
     # Delete old release
     echo "Deleting old release..." >&2
-    response="$(curl -X 'DELETE' -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/repos/${REPO}/releases/${old_release_id}")"
+    response="$(curl -v -X 'DELETE' -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/repos/${REPO}/releases/${old_release_id}")"
     echo "${response}" >&2
 fi
 
 # Create release
 echo "Creating release..." >&2
-response="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" --data "{ \"tag_name\": \"${tag}\", \"name\": \"${tag}\", \"body\": \"${hash}\" }" "https://api.github.com/repos/${REPO}/releases")"
+response="$(curl -v -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" --data "{ \"tag_name\": \"${tag}\", \"name\": \"${tag}\", \"body\": \"${hash}\" }" "https://api.github.com/repos/${REPO}/releases")"
 echo "${response}" >&2
 upload_url="$(echo "${response}" | jq -r '.upload_url' | sed 's/{?name,label}$//g')"
 echo "Upload url: ${upload_url}" >&2
@@ -34,6 +34,6 @@ for file in "$@"; do
     name="$(basename "${file}")"
     extension="${name##*.}"
     name="${name%.*}-${tag}.${extension}"
-    response="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Content-Type: $(file -b --mime-type "${file}")" --data-binary "@${file}" "${upload_url}?name=${name}")"
+    response="$(curl -v -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Content-Type: $(file -b --mime-type "${file}")" --data-binary "@${file}" "${upload_url}?name=${name}")"
     echo "${response}" >&2
 done
