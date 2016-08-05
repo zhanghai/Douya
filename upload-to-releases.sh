67dabc19ea58af146d52399481c07da75f13cb7d
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-08-05 12:18:07 +0800

    Fix body in upload-to-releases.

--- upload-to-releases.sh
+++ upload-to-releases.sh
@@ -33,7 +33,7 @@ fi
 
 # Create release
 echo "Creating release..." >&2
-response="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H 'Content-Type: application/json' --data "{ \"tag_name\": \"${tag}\", \"name\": \"${tag}\", \"body\": \"${hash}\" }" "https://api.github.com/repos/${repo}/releases")"
+response="$(curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H 'Content-Type: application/json' --data "{ \"tag_name\": \"${tag}\", \"name\": \"${tag}\", \"body\": \"${body}\" }" "https://api.github.com/repos/${repo}/releases")"
 echo "${response}" >&2
 upload_url="$(echo "${response}" | jq -r '.upload_url' | sed 's/{?name,label}$//g')"
 echo "Upload url: ${upload_url}" >&2
