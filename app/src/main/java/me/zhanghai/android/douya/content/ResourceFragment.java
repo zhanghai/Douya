3b3a54bdcef2783fdd12ec94d028398cb99539bf
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-10-30 10:44:07 +0800

    [Refactor] Remove unused code.

--- app/src/main/java/me/zhanghai/android/douya/content/ResourceFragment.java
+++ app/src/main/java/me/zhanghai/android/douya/content/ResourceFragment.java
@@ -5,33 +5,6 @@
 
 package me.zhanghai.android.douya.content;
 
-import android.accounts.Account;
-import android.os.Bundle;
-
-import me.zhanghai.android.douya.account.util.AccountUtils;
 import me.zhanghai.android.douya.app.TargetedRetainedFragment;
-import me.zhanghai.android.douya.util.FragmentUtils;
 
-public class ResourceFragment extends TargetedRetainedFragment {
-
-    // Not static because we are to be subclassed.
-    protected final String KEY_PREFIX = getClass().getName() + '.';
-
-    private final String EXTRA_ACCOUNT = KEY_PREFIX + "account";
-
-    private Account mAccount;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Bundle arguments = FragmentUtils.ensureArguments(this);
-        if (arguments.containsKey(EXTRA_ACCOUNT)) {
-            mAccount = arguments.getParcelable(EXTRA_ACCOUNT);
-        } else {
-            mAccount = AccountUtils.getActiveAccount();
-        }
-    }
-
-    // TODO
-}
+public class ResourceFragment extends TargetedRetainedFragment {}
