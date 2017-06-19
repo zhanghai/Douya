f0cee0380f7f0297cf990419c56a39a0cf936d3f
Zhang Hai <dreaming.in.code.zh@gmail.com>
2017-06-19 20:47:00 +0800

    [Feature] Better SSIV criteria.

--- app/src/main/java/me/zhanghai/android/douya/gallery/ui/GalleryAdapter.java
+++ app/src/main/java/me/zhanghai/android/douya/gallery/ui/GalleryAdapter.java
@@ -223,12 +223,14 @@ public class GalleryAdapter extends ViewStatePagerAdapter {
         if (TextUtils.equals(imageInfo.mimeType, "image/gif")) {
             return false;
         }
-        if (imageInfo.width == 0 || imageInfo.height == 0) {
+        if (imageInfo.width <= 0 || imageInfo.height <= 0) {
             return false;
         }
-        float ratio = (float) imageInfo.width / imageInfo.height;
-        if (ratio > 0.5 && ratio < 2) {
-            return false;
+        if (imageInfo.width > 2048 || imageInfo.height > 2048) {
+            float ratio = (float) imageInfo.width / imageInfo.height;
+            if (ratio > 0.5 && ratio < 2) {
+                return false;
+            }
         }
         return true;
     }
