65b53eebef87a737b3e757a49ace3c734f9a3241
Zhang Hai <dreaming.in.code.zh@gmail.com>
2017-06-20 13:17:20 +0800

    [Fix] Fix crash on recreation.

--- app/src/main/java/me/zhanghai/android/douya/ui/ViewStatePagerAdapter.java
+++ app/src/main/java/me/zhanghai/android/douya/ui/ViewStatePagerAdapter.java
@@ -68,6 +68,7 @@ public abstract class ViewStatePagerAdapter extends PagerAdapter {
     @Override
     public void restoreState(Parcelable state, ClassLoader loader) {
         Bundle bundle = (Bundle) state;
+        bundle.setClassLoader(loader);
         int size = bundle.getInt(STATE_VIEW_STATES_SIZE);
         for (int i = 0; i < size; ++i) {
             mViewStates.put(i, bundle.getSparseParcelableArray(makeViewStateKey(i)));
