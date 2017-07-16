1a4a181d14e5e3ffb9f63d7e87eb2bfcdad51385
Zhang Hai <dreaming.in.code.zh@gmail.com>
2017-07-16 23:39:30 +0800

    [Fix] Don't consume error response body in authentication error handling.

--- app/src/main/java/me/zhanghai/android/douya/network/api/ApiAuthenticator.java
+++ app/src/main/java/me/zhanghai/android/douya/network/api/ApiAuthenticator.java
@@ -7,8 +7,11 @@ package me.zhanghai.android.douya.network.api;
 
 import android.accounts.Account;
 
+import java.io.IOException;
+
 import me.zhanghai.android.douya.network.AndroidAuthenticator;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class ApiAuthenticator extends AndroidAuthenticator {
 
@@ -18,8 +21,16 @@ public class ApiAuthenticator extends AndroidAuthenticator {
 
     @Override
     public boolean shouldRetryAuthentication(Response response) {
+        // Don't consume the response body so that it can later be read by others.
+        ResponseBody responseBody;
+        try {
+            responseBody = response.peekBody(Long.MAX_VALUE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
         //noinspection ThrowableInstanceNeverThrown
-        ApiError apiError = new ApiError(response);
+        ApiError apiError = new ApiError(response, responseBody);
         switch (apiError.code) {
             case ApiContract.Response.Error.Codes.Token.INVALID_ACCESS_TOKEN:
             case ApiContract.Response.Error.Codes.Token.ACCESS_TOKEN_HAS_EXPIRED:
