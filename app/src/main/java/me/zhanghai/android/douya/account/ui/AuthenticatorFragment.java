1348cd9cef5b429ec156991656e54dd9d742388b
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-01-02 00:18:03 -0800

    [Fix] Fix imeActionId lint warning.

--- app/src/main/java/me/zhanghai/android/douya/account/ui/AuthenticatorFragment.java
+++ app/src/main/java/me/zhanghai/android/douya/account/ui/AuthenticatorFragment.java
@@ -133,8 +133,7 @@ public class AuthenticatorFragment extends Fragment implements AuthenticateReque
         mPasswordEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.ime_login || id == EditorInfo.IME_ACTION_DONE
-                        || id == EditorInfo.IME_NULL) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_ACTION_UNSPECIFIED) {
                     authenticate();
                     return true;
                 }
