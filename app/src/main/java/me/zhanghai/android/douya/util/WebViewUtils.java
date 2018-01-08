e815c4bedd1282d994ed7b6f1535c4ad58908875
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-01-08 02:32:56 -0800

    [Refactor] Fix comment.

--- app/src/main/java/me/zhanghai/android/douya/util/WebViewUtils.java
+++ app/src/main/java/me/zhanghai/android/douya/util/WebViewUtils.java
@@ -44,7 +44,7 @@ public class WebViewUtils {
         boolean allowMultiple = fileChooserParams.getMode()
                 == WebChromeClient.FileChooserParams.MODE_OPEN_MULTIPLE;
         String[] acceptTypes = fileChooserParams.getAcceptTypes();
-        // Fix for WebView accept types bug.
+        // Work around for WebView accept types bug.
         if (acceptTypes != null && acceptTypes.length == 1 && acceptTypes[0].indexOf(',') != -1) {
             acceptTypes = acceptTypes[0].split(",");
         }
