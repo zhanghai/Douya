37c67f32643167cc379237530059dea066ec3a34
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-01-12 20:25:34 -0800

    [Feature] Use close for multi-task WebViewActivity.

--- app/src/main/java/me/zhanghai/android/douya/ui/WebViewActivity.java
+++ app/src/main/java/me/zhanghai/android/douya/ui/WebViewActivity.java
@@ -311,6 +311,9 @@ public class WebViewActivity extends AppCompatActivity {
     }
 
     private void setupToolbar() {
+        if (Settings.CREATE_NEW_TASK_FOR_WEBVIEW.getValue()) {
+            mToolbar.setNavigationIcon(R.drawable.close_icon_white_24dp);
+        }
         setSupportActionBar(mToolbar);
         updateToolbarTitleAndSubtitle();
     }
