262134834b6509e519ec96cfb59127f4e1a2919e
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-03-19 16:03:01 -0700

    [Fix] Fix CardView negative margin off-by-one-pixel for good.

--- app/src/main/java/me/zhanghai/android/douya/ui/FriendlyCardView.java
+++ app/src/main/java/me/zhanghai/android/douya/ui/FriendlyCardView.java
@@ -7,11 +7,13 @@ package me.zhanghai.android.douya.ui;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.TintTypedArray;
 import android.util.AttributeSet;
 
 import me.zhanghai.android.douya.R;
+import me.zhanghai.android.douya.util.ViewUtils;
 
 /**
  * A friendly card view that has consistent padding across API levels.
@@ -50,5 +52,29 @@ public class FriendlyCardView extends CardView {
 
         // User should never click through a card.
         setClickable(true);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        // Fix off-by-one-pixel negative margin.
+        Resources resources = getResources();
+        int cardShadowHorizontalMargin = resources.getDimensionPixelOffset(
+                R.dimen.card_shadow_horizontal_margin);
+        if (ViewUtils.getMarginLeft(this) == cardShadowHorizontalMargin) {
+            ViewUtils.setMarginLeft(this, getContentPaddingLeft() - getPaddingLeft());
+        }
+        if (ViewUtils.getMarginRight(this) == cardShadowHorizontalMargin) {
+            ViewUtils.setMarginRight(this, getContentPaddingRight() - getPaddingRight());
+        }
+        int cardShadowVerticalMargin = resources.getDimensionPixelOffset(
+                R.dimen.card_shadow_vertical_margin);
+        if (ViewUtils.getMarginTop(this) == cardShadowVerticalMargin) {
+            ViewUtils.setMarginTop(this, getContentPaddingTop() - getPaddingTop());
+        }
+        if (ViewUtils.getMarginBottom(this) == cardShadowVerticalMargin) {
+            ViewUtils.setMarginBottom(this, getContentPaddingBottom() - getPaddingBottom());
+        }
     }
 }
