e43319a1e02a9c0f5b1b01a3c26e75be98c18c3a
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-03-28 17:08:36 -0700

    [Fix] Fix NPE.

--- app/src/main/java/me/zhanghai/android/douya/gallery/ui/GalleryFragment.java
+++ app/src/main/java/me/zhanghai/android/douya/gallery/ui/GalleryFragment.java
@@ -186,6 +186,9 @@ public class GalleryFragment extends Fragment {
     }
 
     private void updateOptionsMenu() {
+        if (mSaveMenuItem == null || mShareMenuItem == null) {
+            return;
+        }
         boolean hasFile = mAdapter.getFile(mViewPager.getCurrentItem()) != null;
         mSaveMenuItem.setEnabled(hasFile);
         mShareMenuItem.setEnabled(hasFile);
