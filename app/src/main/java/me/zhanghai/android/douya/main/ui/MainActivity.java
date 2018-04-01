2d2f4a7e0993810048129130485e1bfe44759931
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-04-01 00:56:39 -0700

    [Fix] Fix crash when refresh after rotation.

--- app/src/main/java/me/zhanghai/android/douya/main/ui/MainActivity.java
+++ app/src/main/java/me/zhanghai/android/douya/main/ui/MainActivity.java
@@ -100,6 +100,8 @@ public class MainActivity extends AppCompatActivity implements NavigationFragmen
         } else {
             mMainFragment = FragmentUtils.findById(this, R.id.container);
             mNotificationListFragment = FragmentUtils.findById(this, R.id.notification_list_drawer);
+            mDoumailUnreadCountFragment = FragmentUtils.findByTag(this,
+                    FRAGMENT_TAG_DOUMAIL_UNREAD_COUNT);
         }
     }
 
