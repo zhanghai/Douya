fbab3730ba5c4b29965095103abff8c17c082a6f
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-04-05 01:18:08 -0700

    [Fix] Fix crash when no simple user.

--- app/src/main/java/me/zhanghai/android/douya/profile/content/ProfileResource.java
+++ app/src/main/java/me/zhanghai/android/douya/profile/content/ProfileResource.java
@@ -378,9 +378,11 @@ public class ProfileResource extends TargetedRetainedFragment implements UserRes
     public void notifyChangedIfLoaded() {
         getListener().onChanged(getRequestCode(),
                 getUser(),
-                mBroadcastListResource.get(),
-                mFollowingListResource.get(), mDiaryListResource.get(),
-                mUserItemListResource.get(), mReviewListResource.get());
+                mBroadcastListResource != null ? mBroadcastListResource.get() : null,
+                mFollowingListResource != null ? mFollowingListResource.get() : null,
+                mDiaryListResource != null ? mDiaryListResource.get() : null,
+                mUserItemListResource != null ? mUserItemListResource.get() : null,
+                mReviewListResource != null ? mReviewListResource.get() : null);
     }
 
     private void notifyError(ApiError error) {
