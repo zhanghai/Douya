0721451955ed7475d7996c39bb1db8eac23b88d3
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-04-16 15:04:43 -0700

    [Fix] Check for account in UriHandlerActivity.

--- app/src/main/java/me/zhanghai/android/douya/link/UriHandlerActivity.java
+++ app/src/main/java/me/zhanghai/android/douya/link/UriHandlerActivity.java
@@ -5,22 +5,35 @@
 
 package me.zhanghai.android.douya.link;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import me.zhanghai.android.douya.account.util.AccountUtils;
+
 public class UriHandlerActivity extends AppCompatActivity {
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, UriHandlerActivity.class);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Uri uri = getIntent().getData();
-        if (uri != null) {
-            UriHandler.open(uri, this);
+        if (uri == null) {
+            finish();
         }
 
+        if (!AccountUtils.ensureActiveAccountAvailability(this)) {
+            finish();
+        }
+
+        UriHandler.open(uri, this);
         finish();
     }
 }
