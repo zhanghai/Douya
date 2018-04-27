bffc064b12a2f3f3dfe00143f99319980e6b8ca6
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-04-27 14:58:23 -0700

    [Fix] Add back changed listener for send broadcast fragment.

--- app/src/main/java/me/zhanghai/android/douya/broadcast/ui/SendBroadcastFragment.java
+++ app/src/main/java/me/zhanghai/android/douya/broadcast/ui/SendBroadcastFragment.java
@@ -19,7 +19,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -213,6 +215,16 @@ public class SendBroadcastFragment extends Fragment
         if (savedInstanceState == null) {
             mTextEdit.setText(mExtraText);
         }
+        mTextEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                mChanged = true;
+            }
+        });
         mAttachmentLayout.setOnRemoveImageListener(this::removeImage);
         bindAttachmentLayout();
         TooltipUtils.setup(mAddImageButton);
