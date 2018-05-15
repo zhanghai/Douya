9ce48c30f97cac162db57a87e92acf4ee5f0f10e
Zhang Hai <dreaming.in.code.zh@gmail.com>
2018-05-14 19:08:35 -0700

    [Fix] Add Book to ItemActivities.

--- app/src/main/java/me/zhanghai/android/douya/item/ui/ItemActivities.java
+++ app/src/main/java/me/zhanghai/android/douya/item/ui/ItemActivities.java
@@ -8,9 +8,11 @@ package me.zhanghai.android.douya.item.ui;
 import android.content.Context;
 import android.content.Intent;
 
+import me.zhanghai.android.douya.network.api.info.frodo.Book;
 import me.zhanghai.android.douya.network.api.info.frodo.CollectableItem;
 import me.zhanghai.android.douya.network.api.info.frodo.Movie;
 import me.zhanghai.android.douya.network.api.info.frodo.Music;
+import me.zhanghai.android.douya.network.api.info.frodo.SimpleBook;
 import me.zhanghai.android.douya.network.api.info.frodo.SimpleMovie;
 import me.zhanghai.android.douya.network.api.info.frodo.SimpleMusic;
 
@@ -19,7 +21,11 @@ public class ItemActivities {
     private ItemActivities() {}
 
     public static Intent makeIntent(CollectableItem item, Context context) {
-        if (item instanceof Movie) {
+        if (item instanceof Book) {
+            return BookActivity.makeIntent((Book) item, context);
+        } else if (item instanceof SimpleBook) {
+            return BookActivity.makeIntent((SimpleBook) item, context);
+        } else if (item instanceof Movie) {
             return MovieActivity.makeIntent((Movie) item, context);
         } else if (item instanceof SimpleMovie) {
             return MovieActivity.makeIntent((SimpleMovie) item, context);
@@ -29,6 +35,8 @@ public class ItemActivities {
             return MusicActivity.makeIntent((SimpleMusic) item, context);
         } else {
             switch (item.getType()) {
+                case BOOK:
+                    return BookActivity.makeIntent(item.id,context);
                 case MOVIE:
                 case TV:
                     return MovieActivity.makeIntent(item.id, context);
