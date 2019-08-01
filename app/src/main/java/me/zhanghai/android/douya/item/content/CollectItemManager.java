09db3652925d40431cf1fb07e0899a72a91be226
hungphd <hungphd@iastate.edu>
2019-08-01 11:43:51 -0500

    Change method name 'write' to 'addCollectItemWriter'

--- app/src/main/java/me/zhanghai/android/douya/item/content/CollectItemManager.java
+++ app/src/main/java/me/zhanghai/android/douya/item/content/CollectItemManager.java
@@ -31,10 +31,10 @@ public class CollectItemManager extends ResourceWriterManager<CollectItemWriter>
                 shareToBroadcast, shareToWeibo, shareToWeChatMoments, this), context);
     }
 
-    public void write(CollectableItem item, ItemCollectionState state, int rating,
-                      List<String> tags, String comment, List<Long> gamePlatformIds,
-                      boolean shareToBroadcast, boolean shareToWeibo, boolean shareToWeChatMoments,
-                      Context context) {
+    public void addCollectItemWriter(CollectableItem item, ItemCollectionState state, int rating,
+                                     List<String> tags, String comment, List<Long> gamePlatformIds,
+                                     boolean shareToBroadcast, boolean shareToWeibo, boolean shareToWeChatMoments,
+                                     Context context) {
         add(new CollectItemWriter(item, state, rating, tags, comment, gamePlatformIds,
                 shareToBroadcast, shareToWeibo, shareToWeChatMoments, this), context);
     }
