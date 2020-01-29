8a6c35301d86334aef3ba324cf15ddbb9e5dd26b
Hai Zhang <dreaming.in.code.zh@gmail.com>
2020-01-29 10:03:32 -0800

    [Feature] Update dependencies.

--- app/src/main/java/me/zhanghai/android/douya/main/MainActivity.kt
+++ app/src/main/java/me/zhanghai/android/douya/main/MainActivity.kt
@@ -7,6 +7,7 @@ package me.zhanghai.android.douya.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import me.zhanghai.android.douya.account.app.ensureActiveAccount
 import me.zhanghai.android.douya.app.accountManager
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         // TODO
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
-                add(android.R.id.content, TimelineFragment())
+                add<TimelineFragment>(android.R.id.content)
             }
         }
     }
